04797c88317d2b6bf36951775441888c1a4b9e96
jbt <jbt@405INS>
2021-12-07 14:57:19 +0200

    aaa

--- 7-12-21-classes-eldar/src/c/PersonTest.java
+++ 7-12-21-classes-eldar/src/c/PersonTest.java
@@ -3,6 +3,17 @@ package c;
 public class PersonTest {
 
 	public static void main(String[] args) {
+		
+		// creating object using constructor 1
+		Person p1 = new Person();
+		
+		// creating object using constructor 2
+		Person p2 = new Person(111, "Avi", 32);
+		
+		// print the name of p2 using a get method:
+		System.out.println(p2.getName());
+		
+		
 	}
 
 }
---
+++ 7-12-21-classes-eldar/src/d/PersonTest.java
@@ -0,0 +1,17 @@
+package d;
+
+import a.Point;
+import b.Car;
+import c.Person;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		
+		Person person = new Person();
+		Point point = new Point();
+		Car car;
+
+	}
+
+}
