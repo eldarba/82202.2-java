32dbc170be106097a876362d726c49dc7377235e
jbt <jbt@405INS>
2021-12-13 13:17:05 +0200

    eee

---
+++ 13-12-21-flow-control-eldar/src/e/loops/Demo1While3.java
@@ -0,0 +1,34 @@
+package e.loops;
+
+import java.util.Scanner;
+
+public class Demo1While3 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int password = 123;
+		System.out.print("enter password: ");
+		int input = sc.nextInt(); // input operation
+
+		// ======================================
+		int attempts = 1;
+		while (input != password && attempts < 3) {
+			attempts++;
+			System.out.print("wrong password, enter again: ");
+			input = sc.nextInt(); // input operation
+		}
+		// ======================================
+
+		sc.close();
+		
+		if(input == password) {
+			System.out.println("you are logged in");
+		}else {
+			System.out.println("login failed");
+		}
+
+	}
+
+}
