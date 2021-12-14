e539ee8326e3b191e894f49d1ca3428a61ad88a9
jbt <jbt@405INS>
2021-12-14 11:41:15 +0200

    14-12-21-arrays-eldar

---
+++ 14-12-21-arrays-eldar/src/c/Demo2.java
@@ -0,0 +1,15 @@
+package c;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+
+		String[] modes = { "ON", "OFF", "STAND_BY", "SLEEP" };
+
+		int index = (int) (Math.random() * modes.length);
+		System.out.println(modes[index]);
+		
+
+	}
+
+}
