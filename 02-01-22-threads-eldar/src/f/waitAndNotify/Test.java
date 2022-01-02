5ae41a450a7ed387dd41c977013972d8f9186eb2
jbt <jbt@405INS>
2022-01-02 16:09:23 +0200

    wait and notify

--- 02-01-22-threads-eldar/src/f/waitAndNotify/Test.java
+++ 02-01-22-threads-eldar/src/f/waitAndNotify/Test.java
@@ -8,8 +8,10 @@ public class Test {
 		p1.start();
 		
 		Consumer c1 = new Consumer("c1", stack);
-		Thread.sleep(1000);
+		Consumer c2 = new Consumer("c2", stack);
+		Thread.sleep(2000); // main waits before starting the consumers
 		c1.start();
+		c2.start();
 
 	}
 
