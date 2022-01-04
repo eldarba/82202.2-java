cb54aa469326ef5d31a2c591e42ba1a3dd398b08
jbt <jbt@405INS>
2022-01-04 11:17:43 +0200

    aaa

---
+++ 04-01-22-jdbc-eldar/src/a/DemoJDBCDrivers.java
@@ -0,0 +1,25 @@
+package a;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class DemoJDBCDrivers {
+
+	public static void main(String[] args) {
+		
+		// to get available jdbc drivers use java.sql.DriverManager
+		Enumeration<Driver> drivers = DriverManager.getDrivers();
+		
+		// now we iterate over the Enumeration and print the drivers
+		System.out.println("available jdbc drivers:");
+		while(drivers.hasMoreElements()) {
+			Driver driver = drivers.nextElement();
+			System.out.println(driver);
+		}
+		System.out.println("=========================");
+		
+
+	}
+
+}
