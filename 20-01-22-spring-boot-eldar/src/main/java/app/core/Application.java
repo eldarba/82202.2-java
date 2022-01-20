9d760166914decbc0806c41af32970b830ca57b0
Eldar <eldarba@gmail.com>
2022-01-20 15:44:03 +0200

    aaa

--- 20-01-22-spring-boot-eldar/src/main/java/app/core/Application.java
+++ 20-01-22-spring-boot-eldar/src/main/java/app/core/Application.java
@@ -1,8 +1,10 @@
 package app.core;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import app.core.beans.Person;
 
@@ -13,16 +15,23 @@ public class Application {
 		
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
 		
-		Person p1 = ctx.getBean(Person.class);
-		Person p2 = ctx.getBean(Person.class);
+		Person p1 = ctx.getBean("person", Person.class);
+		Person p2 = ctx.getBean("x", Person.class);
 		
 		p1.setName("Ann");
-		p2.setName("Dan");
 		
 		System.out.println(p1);
 		System.out.println(p2);
 		
 		
+	}
+	
+	@Bean
+	public Person x(@Value("${x.name: defaultName}") String name) {
+		Person p = new Person();
+		p.setId(100);
+		p.setName(name);
+		return p;
 	}
 
 }
--- 20-01-22-spring-boot-eldar/src/main/java/app/core/beans/Person.java
+++ 20-01-22-spring-boot-eldar/src/main/java/app/core/beans/Person.java
@@ -3,33 +3,20 @@ package app.core.beans;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 @Component
 @Scope("prototype")
+@ToString
+@Getter @Setter
 public class Person {
 
 	private int id;
 	private String name;
 
-	// get, set, toString:
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public String toString() {
-		return "Person [id=" + id + ", name=" + name + "]";
-	}
+	
+	
 
 }
